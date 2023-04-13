600984a59afe0526df0e5dd7c5ce8a4bc37aedaa
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2023-04-12 23:04:37 -0300

    up

--- PopUpView.swift
+++ PopUpView.swift
@@ -8,68 +8,88 @@
 import SwiftUI
 
 
-
-//class PopUpState: ObservableObject {
-//    @Published var exibirPopup = false
-//    static let shared = PopUpState()
-//}
-//
-//class ShowNavigation: ObservableObject {
-//    @Published var exibirNavigation = false
-//    static let shared = ShowNavigation()
-//}
-
 struct PopUpView: View {
     @State private var exibirPopup = false
     @Environment(\.presentationMode) var presentationMode
     @State private var showNavigation = false
     
+    @State private var flow: [ElementContentModel] = [
+        ElementContentModel(image: "Elemento 1", popUpDescription: "TAL"),
+        ElementContentModel(image: "Elemento 2", popUpDescription: "Outra descrição"),
+        ElementContentModel(image: "Elemento 3", popUpDescription: "Mais uma descrição"),
+    ]
     
+    
+    @State private var currentIndex = 0
     
     var body: some View {
         ZStack {
             VStack {
-                Button("Exibir Popup") {
+                
+                Button(flow[currentIndex].image) {
                     exibirPopup.toggle()
+                    
                 }
-                .padding(.bottom, 800)
+               
+                
+                HStack {
+                    ForEach(Array(flow.enumerated()), id: \.1.id) { index, element in
+                           Button(action: {
+                             
+                           }) {
+                               
+                               var isCurrentIndex = index == currentIndex;
+                               var isPastElement = index < ((currentIndex + 1) - 1);
+                               var isCurrentIndexDifferentZero = currentIndex != 0;
+                               
+                               
+                              
+                               if ((isCurrentIndex || isPastElement) && isCurrentIndexDifferentZero) {
+                                   Text(element.image)
+                               }
+                               
+                           }
+                       }
+                }
             }
             
             if exibirPopup {
-                
                 Color.pink
                     .frame(width: 700, height: 345)
                     .cornerRadius(10)
                     .overlay(
                         VStack {
-                            Text("Este é um exemplo de jdjjdjdjjdjdjjdjdjdjjjdjd popupaaaaaaaanajdjdjjsjjdjajajjajjsjsjsjsjsjjjjdjjdjjdjdjjdjjdjjdjdjjdjdjjdjdjjdjdjjdjjdjdjdjjdjd.").lineLimit(7)
+                            Text(flow[currentIndex].popUpDescription).lineLimit(7)
                                 .foregroundColor(.black)
                             HStack {
                                 Button("Sair") {
                                     exibirPopup.toggle()
                                 }
                                 Button("Próxima") {
+                                    if currentIndex < flow.count - 1 {
+                                        currentIndex += 1
+                                    } else {
+                                        currentIndex = 0
+                                    }
                                     
-                                    showNavigation = true
-                                    
+                                    exibirPopup.toggle()
                                 }
                                 .fullScreenCover(isPresented:  $showNavigation) {
                                     FitaView()
-                                    
                                 }
-                                
                             }
-                            
                         }
-                        
                     )
                     .offset(x: 0, y:400)
             }
-            
         }
     }
-    
 }
+
+
+
+
+
 
 struct alertaaview_Previews: PreviewProvider {
     static var previews: some View {
