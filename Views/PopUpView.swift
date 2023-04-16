0f579632d9e79ec6c5784f4a016131f30ccb951d
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2023-04-16 11:56:12 -0300

    adding scrolview in pop up

--- Views/PopUpView.swift
+++ Views/PopUpView.swift
@@ -8,10 +8,13 @@ import SwiftUI
 
 
 struct PopUpView: View {
-    //
-    //    @State var screenWidth: CGFloat = UIScreen.main.bounds.width
-    //       @State var screenHeight: CGFloat = UIScreen.main.bounds.height
-    //
+    
+    
+    var fontR = UIFont()
+    var fontB = UIFont()
+    var fontM = UIFont()
+    var fontR28 = UIFont()
+    var fontM32 = UIFont()
     
     @State private var exibirPopup = false
     @Environment(\.presentationMode) var presentationMode
@@ -50,10 +53,10 @@ struct PopUpView: View {
                         HStack {
                             ForEach(Array(flow.enumerated()), id: \.1.id) { index, element in
                                 
-                               
+                                
                                 let isPastElement = index < (currentIndex);
                                 let isCurrentShowItem = index == currentIndex - 1;
-                               
+                                
                                 
                                 Button(action: {
                                     if (isPastElement && isCurrentShowItem) {
@@ -68,7 +71,7 @@ struct PopUpView: View {
                                     
                                 }.opacity(isCurrentShowItem ? 1.0 : 0.5)
                             }
-
+                            
                         }
                         
                         Spacer()
@@ -84,52 +87,82 @@ struct PopUpView: View {
                 }
                 
                 if exibirPopup {
+                    
+                    
                     GeometryReader { geometry in
-                        VStack(alignment: .center) {
-                            Image("popup")
-                                .resizable()
-                                .frame(width: geometry.size.width * 0.8, height:geometry.size.height * 0.60)
-                                .overlay(
-                                    VStack {
-                                        
-                                        Text("Para colocar uma borda personalizada em uma VStack, você pode adicionar um overlay com um retângulo estilizado usando a propriedade stroke do Shape. Aqui está um exemplo:dicionar um overlay com um retângulo estilizado usando a propriedade stroke do Shape. Aqui está um exemplo:  ").lineLimit(7)
-                                            .font(.title3)
+                        
+                        Image("popup")
+                            .resizable()
+                            .frame(width: geometry.size.width * 0.8, height:geometry.size.height * 0.8)
+                            .overlay(
+                                VStack {
+                                    //                                        (flow[currentIndex].popUpDescription)
+                                    ScrollView {
+                                        Text("none of the content here belongs to me besides the editing :) all rights go to doja and her directors/producer,none of the content here belongs to me besides the editing :) all rights go to doja and her directors/producerss oiiiiii eaiiiiiinone of the content here belongs to me besides the editing :) all rights go to doja and her directors/producer,none of the content here belongs to me besides the editing :) all rights go to doja and her directors/producerss oiiiiii eaiiiiii")
+                                            .font(Font(fontR28))
+                                            .lineLimit(7)
                                             .foregroundColor(.black)
-                            
-                                    
-                                        HStack {
-                                            Button("Sair") {
-                                                exibirPopup.toggle()
-                                            }
-                                            Spacer()
-                                            Button("Próxima") {
-                                                // Se currentIndex for menor que o tamanho da lista
-                                                if currentIndex < flow.count - 1 {
-                                                    currentIndex += 1
-                                                } else {
-                                                    currentIndex = 0
-                                                }
-                                                
-                                                exibirPopup.toggle()
-                                            }
-                                            .fullScreenCover(isPresented:  $showNavigation) {
-                                                OnboardingView()
-                                            }
-                                        }
-                                       
+                                    }
+                                    .frame(maxHeight: geometry.size.height * 0.200)
+                                   
 
-                                    }.frame(width: (geometry.size.width * 0.8) * 0.88, height:(geometry.size.height * 0.60))
-                                )
-                                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                            
-                        }
-                        .position(x: geometry.size.width/2, y: geometry.size.height/1.5)
+                                   
+//                                    HStack(spacing: 580) {
+//                                            Button("Sair") {
+//                                                exibirPopup.toggle()
+//                                            } .font(Font(fontM32))
+//                                            Button("Próxima") {
+//                                                // Se currentIndex for menor que o tamanho da lista
+//                                                if currentIndex < flow.count - 1 {
+//                                                    currentIndex += 1
+//                                                } else {
+//                                                    currentIndex = 0
+//                                                }
+//
+//                                                exibirPopup.toggle()
+//                                            } .font(Font(fontM32))
+//                                                .fullScreenCover(isPresented:  $showNavigation) {
+//                                                    OnboardingView()
+//                                                }
+//                                        }
+                                    //.padding(.horizontal, 60)
+                                   // .padding(.top, 40)
+                                    
+                                    
+                                    
+                                    
+                                }.frame(width: (geometry.size.width * 0.8) * 0.88, height:(geometry.size.height * 0.60) * 0.9)
+                            )
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        
+                            .position(x: geometry.size.width/2, y: geometry.size.height/1.5)
                         
                     }
                 }
             }
         }
         .background(Image("parede").resizable().scaledToFill())
+    }
+    
+    init() {
+        let cfURL = Bundle.main.url(forResource: "PixelOperator-Bold", withExtension: "ttf")! as CFURL
+        
+        CTFontManagerRegisterFontsForURL(cfURL, CTFontManagerScope.process, nil)
+        let cfURL2 = Bundle.main.url(forResource: "PixelOperator", withExtension: "ttf")! as CFURL
+        
+        CTFontManagerRegisterFontsForURL(cfURL2, CTFontManagerScope.process, nil)
+        let cfURL3 = Bundle.main.url(forResource: "PixelOperatorMono8", withExtension: "ttf")! as CFURL
+        CTFontManagerRegisterFontsForURL(cfURL3, CTFontManagerScope.process, nil)
+        
+        fontR = UIFont(name: "PixelOperator-Bold", size:  180)!
+        fontB = UIFont(name: "PixelOperator", size:  80)!
+        fontM = UIFont(name: "PixelOperatorMono8", size: 80)!
+        fontM = UIFont(name: "PixelOperatorMono8", size: 80)!
+        fontR28 = UIFont(name: "PixelOperator", size: 28)!
+        fontM32 = UIFont(name: "PixelOperatorMono8", size: 18)!
+        
+        
+        
     }
 }
 
