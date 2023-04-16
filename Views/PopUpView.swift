f6556b16b2c3872b0b322fdfadcabe0512217668
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2023-04-16 10:16:56 -0300

    add main flow

--- Views/PopUpView.swift
+++ Views/PopUpView.swift
@@ -18,75 +18,92 @@ struct PopUpView: View {
     @State private var showNavigation = false
     
     @State private var flow: [ElementContentModel] = [
-        ElementContentModel(image: "Elemento 1", popUpDescription: "TAL"),
-        ElementContentModel(image: "Elemento 2", popUpDescription: "Outra descrição"),
-        ElementContentModel(image: "Elemento 3", popUpDescription: "Mais uma descrição"),
-    ]
+        ElementContentModel(image: "el1", popUpDescription: "TAL"),
+        ElementContentModel(image: "el2", popUpDescription: "Outra descrição"),
+        ElementContentModel(image: "el3", popUpDescription: "Mais uma descrição"),
+        ElementContentModel(image: "el4", popUpDescription: "final"),
+    ];
     
-    
-    @State private var currentIndex = 0
+    // Controla nossa posicao no fluxo do jogo
+    // 0 -> Estado inicial apresentacao apenas do cabecote
+    // 1 -> Primeira parte da fita
+    @State private var currentIndex = 0;
     
     var body: some View {
         GeometryReader { geometry in
             
             ZStack {
                 
-                
-                Image("quadro")
-                    .resizable()
-                    .frame(width: geometry.size.width * 0.95, height:  geometry.size.height * 0.90)
-                    .position(x: geometry.size.width/2, y: geometry.size.height/2.3)
-                
-                
-                
-                VStack {
+                GeometryReader { geometryVStack in
                     
-                    Button(flow[currentIndex].image) {
-                        exibirPopup.toggle()
-                        
-
-                    }
-                    .frame(width: geometry.size.width, height: geometry.size.height)
-                    .position(x: geometry.size.width/2, y: geometry.size.height/3.5)
-                    
-                    
-
-                    
-                    HStack {
-                        ForEach(Array(flow.enumerated()), id: \.1.id) { index, element in
-                            Button(action: {
-                                
-                            }) {
-                                
-                                var isCurrentIndex = index == currentIndex;
-                                var isPastElement = index < ((currentIndex + 1) - 1);
-                                var isCurrentIndexDifferentZero = currentIndex != 0;
-                                
-                                
-                                if ((isCurrentIndex || isPastElement) && isCurrentIndexDifferentZero) {
-                                    Text(element.image)
-                                }
-                                
+                    VStack {
+                        Spacer().frame(height: 100)
+                        Button(action: {
+                            if (currentIndex == 0) {
+                                exibirPopup.toggle()
                             }
+                            
+                        }) {
+                            Image("popup").resizable().frame(width: 200, height: 200)
+                        }.opacity(currentIndex != 0 ? 0.5 : 1.0)
+                        Spacer().frame(height: 0)
+                        HStack {
+                            ForEach(Array(flow.enumerated()), id: \.1.id) { index, element in
+                                
+                               
+                                let isPastElement = index < (currentIndex);
+                                let isCurrentShowItem = index == currentIndex - 1;
+                               
+                                
+                                Button(action: {
+                                    if (isPastElement && isCurrentShowItem) {
+                                        exibirPopup.toggle()
+                                    }
+                                    
+                                }) {
+                                    
+                                    if (isPastElement) {
+                                        Text(element.image).frame(width: 200, height: 200).background(Image("popup").resizable())
+                                    }
+                                    
+                                }.opacity(isCurrentShowItem ? 1.0 : 0.5)
+                            }
+
                         }
+                        
+                        Spacer()
                     }
+                    .background(
+                        Image("quadro")
+                            .resizable()
+                            .frame(width:geometryVStack.size.width, height:  geometryVStack.size.height )
+                        
+                    )
+                    .frame(maxWidth: geometry.size.width * 0.95, maxHeight:  geometry.size.height * 0.90).position(x: geometry.size.width/2, y: geometry.size.height/2)
+                    
                 }
                 
                 if exibirPopup {
                     GeometryReader { geometry in
                         VStack(alignment: .center) {
-                            Image("sprite_0")
+                            Image("popup")
                                 .resizable()
-                                .frame(width: geometry.size.width * 0.8, height:geometry.size.height * 0.46)
+                                .frame(width: geometry.size.width * 0.8, height:geometry.size.height * 0.60)
                                 .overlay(
-                                    VStack(alignment: .center) {
-                                        Text(flow[currentIndex].popUpDescription).lineLimit(7)
+                                    VStack {
+                                        
+                                        Text("Para colocar uma borda personalizada em uma VStack, você pode adicionar um overlay com um retângulo estilizado usando a propriedade stroke do Shape. Aqui está um exemplo:dicionar um overlay com um retângulo estilizado usando a propriedade stroke do Shape. Aqui está um exemplo:  ").lineLimit(7)
+                                            .font(.title3)
                                             .foregroundColor(.black)
-                                        HStack(alignment: .center) {
+                            
+                                    
+                                        HStack {
                                             Button("Sair") {
                                                 exibirPopup.toggle()
                                             }
+                                            Spacer()
                                             Button("Próxima") {
+                                                // Se currentIndex for menor que o tamanho da lista
                                                 if currentIndex < flow.count - 1 {
                                                     currentIndex += 1
                                                 } else {
@@ -99,29 +116,31 @@ struct PopUpView: View {
                                                 OnboardingView()
                                             }
                                         }
-                                    }
+                                       
+
+                                    }.frame(width: (geometry.size.width * 0.8) * 0.88, height:(geometry.size.height * 0.60))
                                 )
                                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                             
                         }
-                        .position(x: geometry.size.width/2, y: geometry.size.height/1.3)
+                        .position(x: geometry.size.width/2, y: geometry.size.height/1.5)
                         
                     }
                 }
             }
         }
         .background(Image("parede").resizable().scaledToFill())
-        }
     }
-    
-    
-    
-    
-    
-    struct PopUpView_Previews: PreviewProvider {
-        static var previews: some View {
-            PopUpView()
-        }
+}
+
+
+
+
+
+struct PopUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        PopUpView()
     }
-    
+}
+
 
