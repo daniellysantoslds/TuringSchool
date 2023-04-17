d993b18a8e9892f26882b1ef0dd4b9e45d40328f
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2023-04-17 10:49:27 -0300

    adding fonts functions in view

--- Views/IntroductionView.swift
+++ Views/IntroductionView.swift
@@ -8,10 +8,41 @@
 import SwiftUI
 
 struct IntroductionView: View {
+    
+    var fontR = UIFont()
+    var fontB = UIFont()
+    var fontM = UIFont()
+    var fontR28 = UIFont()
+    var fontM32 = UIFont()
+    
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
+    
+    
+    init() {
+        let cfURL = Bundle.main.url(forResource: "PixelOperator-Bold", withExtension: "ttf")! as CFURL
+        
+        CTFontManagerRegisterFontsForURL(cfURL, CTFontManagerScope.process, nil)
+        let cfURL2 = Bundle.main.url(forResource: "PixelOperator", withExtension: "ttf")! as CFURL
+        
+        CTFontManagerRegisterFontsForURL(cfURL2, CTFontManagerScope.process, nil)
+        let cfURL3 = Bundle.main.url(forResource: "PixelOperatorMono8", withExtension: "ttf")! as CFURL
+        CTFontManagerRegisterFontsForURL(cfURL3, CTFontManagerScope.process, nil)
+        
+        fontR = UIFont(name: "PixelOperator-Bold", size:  180)!
+        fontB = UIFont(name: "PixelOperator", size:  80)!
+        fontM = UIFont(name: "PixelOperatorMono8", size: 80)!
+        fontM = UIFont(name: "PixelOperatorMono8", size: 80)!
+        fontR28 = UIFont(name: "PixelOperator", size: 28)!
+        fontM32 = UIFont(name: "PixelOperatorMono8", size: 18)!
+        
+        
+        
+    }
 }
+
 
 struct IntroductionView_Previews: PreviewProvider {
     static var previews: some View {
