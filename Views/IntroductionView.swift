9f63e90158114f5c4d18b4c1353f3dd8e3471e30
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2023-04-17 10:46:07 -0300

    creating IntroductionView

---
+++ Views/IntroductionView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Danielly Santos Lopes da Silva on 17/04/23.
+//
+
+import SwiftUI
+
+struct IntroductionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IntroductionView_Previews: PreviewProvider {
+    static var previews: some View {
+        IntroductionView()
+    }
+}
